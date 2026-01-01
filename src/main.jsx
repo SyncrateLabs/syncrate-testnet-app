61363e211ac7220c784129ba03d6db8d1e24873e
Kizito Chukwuma <kizitoweb3@gmail.com>
2026-01-01 09:41:31 +0100

    Update main.jsx for ConnectKit Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
-import { Web3Modal } from '@web3modal/react';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { sepolia } from 'wagmi/chains';  // Use Sepolia since your contract is there
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { WagmiConfig, createConfig } from 'wagmi'
+import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
+import { sepolia } from 'viem/chains'
 
-const chains = [sepolia];
-const projectId = 'YOUR_WALLET_CONNECT_PROJECT_ID_HERE';  // Replace with your ID from cloud.walletconnect.com
+const chains = [sepolia]
+const projectId = '8e690417846443eb4cdc01723cc9f0d4'
 
-const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
-  publicClient
-});
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
+const config = createConfig(
+  getDefaultConfig({
+    chains,
+    walletConnectProjectId: projectId,
+    appName: 'Syncrate Testnet',
+  
+  }),
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WagmiConfig config={wagmiConfig}>
-      <App />
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <App />
+      </ConnectKitProvider>
     </WagmiConfig>
-    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
   </React.StrictMode>,
-);
+)
